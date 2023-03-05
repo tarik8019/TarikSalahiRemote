a1021eb4d5d89898147727d1d191f0f2b23f46f4
salah <salah@DESKTOP-PIU9VCQ>
2023-03-06 00:31:10 +0100

    inicio el proyecto

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author salah
+ *
+ */
+module TarikSalahi {
+}
---
+++ src/newPackage/TarikSalahiPescar.java
@@ -0,0 +1,20 @@
+package newPackage;
+
+public class TarikSalahiPescar {
+	
+	public String pescar;
+	public String mar;
+	public int numeroDeVecesAlSemana;
+	
+	
+	public TarikSalahiPescar(String pescar, String mar, int numeroDeVecesAlSemana) {
+		super();
+		this.pescar = pescar;
+		this.mar = mar;
+		this.numeroDeVecesAlSemana = numeroDeVecesAlSemana;
+	}
+	
+	
+	
+
+}
